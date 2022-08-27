c073e28767d7e20de92e01bcd06b9424a5765c96
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2022-08-27 23:34:00 +0530

    Added functionaliy => checks for a valid and strong password

--- index.js
+++ index.js
@@ -1,14 +1,60 @@
-let valid={
-    upper_case:["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-    lower_case:[ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-    num:["1","2","3","4","5","6","7","8","9","0"],
-    special_char:["!","@","#","$","%","^","&","*"],
-    pw : document.getElementById("key"),
-}
-
+let cap_lt=document.querySelector(".upper_case");
+let low_lt=document.querySelector(".lower_case");
+let num=document.querySelector(".num");
+let spec_char=document.querySelector(".spec_char");
+let len=document.querySelector(".length");
 let pw = document.getElementById("key");
 
-$("#key").on("keyup",function(){
-    if(valid.pw.value)
-    console.log(valid.pw.value);
-})
+function check(){
+    let upper_case=/[A-Z]/g;
+    if(pw.value.match(upper_case)){
+        cap_lt.classList.remove("invalid");
+        cap_lt.classList.add("valid");
+    }
+    else{
+        cap_lt.classList.remove("valid");
+        cap_lt.classList.add("invalid");
+    }
+
+    let lower_case=/[a-z]/g;
+    if(pw.value.match(lower_case)){
+        low_lt.classList.remove("invalid");
+        low_lt.classList.add("valid");
+    }
+    else{
+        low_lt.classList.remove("valid");
+        low_lt.classList.add("invalid");
+    }
+
+    let numbers=/[0-9]/g;
+    if(pw.value.match(numbers)){
+        num.classList.remove("invalid");
+        num.classList.add("valid");
+    }
+    else{
+        num.classList.remove("valid");
+        num.classList.add("invalid");
+    }
+
+    let char=/["!","@","#","$","%","^","&","*"]/g;
+    if(pw.value.match(char)){
+        spec_char.classList.remove("invalid");
+        spec_char.classList.add("valid");
+    }
+    else{
+        spec_char.classList.remove("valid");
+        spec_char.classList.add("invalid");
+    }
+
+    let l=8;
+    if(pw.value.length>l){
+        len.classList.remove("invalid");
+        len.classList.add("valid");
+    }
+    else{
+        len.classList.remove("valid");
+        len.classList.add("invalid");
+    }
+}
+
+document.querySelector("#key").addEventListener("keyup",check)

7498cb1f1b014de7545aede0677341ede9c502cd
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2022-08-27 21:37:47 +0530

    found a way to get value from input

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ let valid={
     lower_case:[ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
     num:["1","2","3","4","5","6","7","8","9","0"],
     special_char:["!","@","#","$","%","^","&","*"],
+    pw : document.getElementById("key"),
 }
 
+let pw = document.getElementById("key");
+
+$("#key").on("keyup",function(){
+    if(valid.pw.value)
+    console.log(valid.pw.value);
+})
